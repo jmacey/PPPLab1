1f0b471391062b1e437f91fb5c15b4490e9ebd92
jmacey <jmacey@bournemouth.ac.uk>
2016-10-04 17:24:34 +0100

    Initial commit of project

---
+++ hello.s
@@ -0,0 +1,40 @@
+	.file	"hello.c"
+	.text
+	.globl	main
+	.align	16, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# BB#0:
+	pushq	%rbp
+.Ltmp2:
+	.cfi_def_cfa_offset 16
+.Ltmp3:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+.Ltmp4:
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	leaq	.L.str, %rdi
+	movl	$0, -4(%rbp)
+	movb	$0, %al
+	callq	printf
+	movl	$0, %ecx
+	movl	%eax, -8(%rbp)          # 4-byte Spill
+	movl	%ecx, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	ret
+.Ltmp5:
+	.size	main, .Ltmp5-main
+	.cfi_endproc
+
+	.type	.L.str,@object          # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"hello world!\n"
+	.size	.L.str, 14
+
+
+	.ident	"clang version 3.4.2 (tags/RELEASE_34/dot2-final)"
+	.section	".note.GNU-stack","",@progbits
